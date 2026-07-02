5f06521924611fe85b4d82ff47b928359a8f967f
Samia <hamadsamia37@gmail.com>
2026-07-02 18:25:16 +0300

    adjusting to use new apis

--- backend/airquality_api/core/urls.py
+++ backend/airquality_api/core/urls.py
@@ -5,12 +5,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from airquality_api.api.views import AirQualityAPIViewSet
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('airquality_api.users.urls')),
     path('api/sensors/', include('airquality_api.sensors.urls')),
     path('api/air-quality/', include('airquality_api.api.urls')),
+    path('api/predict/all', AirQualityAPIViewSet.as_view({'get': 'predict_all'}), name='predict-all'),
+    path('api/predict/all/', AirQualityAPIViewSet.as_view({'get': 'predict_all'}), name='predict-all-slash'),
+    path('api/recommend', AirQualityAPIViewSet.as_view({'get': 'recommend'}), name='recommend-plain'),
+    path('api/recommend/', AirQualityAPIViewSet.as_view({'get': 'recommend'}), name='recommend'),
+    path('api/history/sensor', AirQualityAPIViewSet.as_view({'get': 'history_sensor'}), name='history-sensor'),
+    path('api/history/sensor/', AirQualityAPIViewSet.as_view({'get': 'history_sensor'}), name='history-sensor-slash'),
 ]
 
 if settings.DEBUG:
